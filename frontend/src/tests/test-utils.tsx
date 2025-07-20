4e321d9ce86ec5520c8ef4bb1a3afcf9d8a8dfee
Dreyrk <lucas.rondepierre123@gmail.com>
2025-07-20 15:44:12 +0200

    test ci 5

---
+++ frontend/src/tests/test-utils.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { AuthProvider } from "@/components/providers/auth-provider";
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return <AuthProvider>{children}</AuthProvider>;
+};
+
+const customRender = (ui: React.ReactElement, options = {}) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
