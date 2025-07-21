7a01e7aeed46380b292b20f7e968ad1bb3122673
Dreyrk <lucas.rondepierre123@gmail.com>
2025-07-21 08:06:04 +0200

    fix cookie

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -64,8 +64,8 @@ class AuthController {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
-          sameSite: "lax",
-          domain: process.env.NODE_ENV === "production" ? "mobilitychallenge-production.up.railway.app" : "localhost",
+          sameSite: "none",
+          domain: process.env.NODE_ENV === "production" ? ".railway.app" : "localhost",
         })
         .status(200)
         .json(successResponse({ user }));
@@ -87,7 +87,7 @@ class AuthController {
         .clearCookie("token", {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "lax",
+          sameSite: "none",
         })
         .status(200)
         .json(successResponse(null, { message: "Déconnexion réussie" }));
