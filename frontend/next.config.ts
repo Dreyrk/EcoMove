dbf33071ea369a737972385b2fa2869ac34b39d3
Dreyrk <lucas.rondepierre123@gmail.com>
2025-07-21 08:43:35 +0200

    fix cookie

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["https://mobilitychallenge-production.up.railway.app"],
+  allowedDevOrigins: ["*.railway.app"],
 };
 
 export default nextConfig;
